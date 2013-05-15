f8b313a1d6a5d3a6714b458e21e2b9b1c33213ad
Rogier van het Schip <rogiervanhetschip@dsw.nl>
2013-05-15 17:30:05 +0200

    Removed unneeded methods

--- src/org/resilience/resiliencereader/entities/ArticlesFeed.java
+++ src/org/resilience/resiliencereader/entities/ArticlesFeed.java
@@ -1,17 +1,6 @@
 package org.resilience.resiliencereader.entities;
 
-public enum ArticlesFeed {
-	Full,
-	Energy,
-	Economy,
-	Environment,
-	FoodAndWater,
-	Society;
-	
-	private static final ArticlesFeed[] articlesFeedValues = ArticlesFeed.values();
-	
-	public static ArticlesFeed fromInt(int i)
-	{
-		return articlesFeedValues[i];
-	}
+public enum ArticlesFeed
+{
+   Full, Energy, Economy, Environment, FoodAndWater, Society;
 }
